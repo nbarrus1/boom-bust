9b8561281e0bb0003328b38cd3f2535558bea0d1
Nathan T. Barrus <nbarrus1@gmail.com>
2024-03-12 11:24:42 -0400

    list working: 3/12/2024

--- rscripts/001_digitizetimeseries.R
+++ rscripts/001_digitizetimeseries.R
@@ -8,9 +8,13 @@ rm(list = ls())
 #load library metaDigitise
 
 library(metaDigitise)
+library(tidyverse)
 library(purrr)
 library(here)
 library(magrittr)
+
+theme_set(theme_bw())
+
 #the function metaDigitise is an interactive function that allows you to 1) digitize,
 #and extract data, 2) import data that has already been digitized, or 3) edit alread
 #digitized data.
@@ -34,23 +38,112 @@ library(magrittr)
 
 lit_data_ls<- metaDigitise(dir = here("TimeSeriesPictures_Literature/TimeSeriesFigures/"),
                      summary = FALSE)
-#look at the created list
-lit_data_ls$scatterplot$`040_Witmanetal_EcologicalMonographs_2003_Fig9.jpg`
-
-
 
 #------------------------
 ####Data wrangling: Prep the list data to get in useable format###
 #------------------------
 
 
+######list inspection####
 
-scatterplots <- bind_rows(lit_data_ls$scatterplot, .id = "Figure")
-barplots <- bind_rows(lit_data_ls$mean_error, .id = "Figure") 
+str(lit_data_ls,max.level = 1)
 
-write_csv(barplots, here("output","barplot_raw.csv"))
-barplot_to_scatter <- read_csv(here("output","bar_plot_to_scatter.csv"))
+#we have nested list, first level elements 1) barplots = 8 lists and 
+#2) scatterplots = 145 lists
+
+str(lit_data_ls[[1]], list.len = 4)
+str(lit_data_ls[[2]], list.len = 4)
+
+#at the second level is a list of the data extracted from each plot
+#one problem i for see having is that in the barplots the "id" are years
+#as character (sometimes these id have both a year-mon, or year_location)
+
+#The year only information will have to be split in these cases, 
+#month might also have to be extracted into a different variable
+#the locations can be kept in id
 
 
-all_timeseries <- scatterplots |> 
-  bind_rows(barplot_to_scatter)
+
+#####Get the list into a tibble####
+
+#split the lists into seperate list by bar or scatter plots
+
+box.ls <- lit_data_ls[[1]]
+scatter.ls <- lit_data_ls[[2]]
+
+#make them into a tibble with each plot as a figure
+
+###reorganize the box.plot data so the year bars are now x variables
+###and nest by plot and group, so I have a single time series for everything
+box.tib <-box.ls |> 
+  set_names(names(box.ls)) |> 
+  enframe(name = "plot",value = "ls") |> 
+  unnest(ls) |> 
+  mutate(x = str_sub(id,start = 1L, end = 4L),
+         temp = str_sub(id, start = 7L, end = str_length(id)-1),
+         month = case_when(temp=="Jan"~1,
+                           temp=="Feb"~2,
+                           temp=="Mar"~3,
+                           temp=="Apr"~4,
+                           temp=="May"~5,
+                           temp=="Jun"~6,
+                           temp=="Jul"~7,
+                           temp=="Aug"~8,
+                           temp=="Sep"~9,
+                           temp=="Oct"~10,
+                           temp=="Nov"~11,
+                           temp=="Dec"~12),
+         id = if_else(temp %in% c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"),
+                      true = "", false = paste(temp)),
+         x_variable = "Year") |> 
+  select(-temp) |> 
+  rename(y = mean,
+         y_variable = variable,
+         y_error = error) |> 
+  group_by(plot) |> 
+  mutate(group = match(paste(id),unique(paste(id))),
+         x = if_else(is.na(month), true = as.double(x),
+                               false = as.double(x)+((month-1)/12))) |> 
+  group_by(plot,group) |> 
+  nest(.key = "ls")
+
+
+str(box.tib$ls[1])
+####scatter plot nested by group and plot
+
+scatter.tib <- scatter.ls |>
+  set_names(names(scatter.ls)) |> 
+  enframe(name = "plot",value = "ls") |> 
+  mutate(ls = map(ls,as.data.frame)) |> 
+  unnest(ls) |> 
+  select(-pch,-col) |> 
+  group_by(plot,group) |> 
+  nest(.key = "ls") 
+
+
+#combine the reorganized bar graph data to the scatter plots
+
+lit_data_tib <- scatter.tib |> 
+  bind_rows(box.tib)
+
+#fucntion for plotting
+
+plot_timeseries <- function(df) {
+  df |> 
+    ggplot(aes(x = x, y = y))+
+    geom_line()+
+    geom_point(size = 2, color = "black", shape = 21, fill = "#666666")+
+    theme_bw(base_size = 12) + 
+    theme(
+      axis.ticks.length = unit(.25, "cm"),
+      axis.title.y = element_text(vjust = 2), 
+      plot.margin = unit(c(0,0,0,0), "cm")
+    )
+}
+
+
+
+lit_data_plots <- lit_data_tib |> 
+  mutate(timeseries = map(ls, .f = plot_timeseries))
+
+lit_data_plots$timeseries
